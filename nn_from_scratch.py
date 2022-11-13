f8728bb8f3e362da51ab9bd7e39dfd295e4cf720
CycleBreaker <stochasticgradientdescent4@gmail.com>
2022-11-13 21:12:52 +0300

    publishing the unfinished file

---
+++ nn_from_scratch.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+np.random.seed(0)
+
+
+
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+
+
+
+X , y = spiral_data(1000 , 3)
+
+
+class layer_dense:
+    def __init__(self , n_inputs , n_neurans):
+        self.weights = 0.1 * np.random.rand(n_inputs , n_neurans)
+        self.baises = np.zeros((1,n_neurans))
+
+    def forward(self , input):
+        self.output = np.dot(input , self.weights) + self.baises
+
+
+class Activation_ReLU:
+    def forward(self , input):
+        self.output =  np.maximum(0 , input)
+    
+
+
+
+layer1 = layer_dense(2 , 5)
+act = Activation_ReLU()
+
+layer1.forward(X)
+act.forward(layer1.output)
+print(act.output)
